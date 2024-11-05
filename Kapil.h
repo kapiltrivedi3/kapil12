b2169530249be764c12fd49e4375eace27ff2b18
kapil.trivedi33@gmail.com <kapil.trivedi33@gmail.com>
2024-11-05 12:02:50 +0200

    Add files via upload
    Electronics circuit signal programming, program microchips using  Logic gate operations.
    User just need to  call 'Kapil()' function, importing Kapil header file from any C,C++, Java program and implement electronics circuit signal programming.

---
+++ Kapil.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int Kapil()
+{
+
+    int a[20] = { 1, 0, 1, 0, 1,0,1,0,0,1,0,1,0,1,0,1,0,0,1 };
+    int b[20] = { 0, 1, 1, 0, 0 ,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0};
+    int i, ans, output;
+	
+	for(int i=0; i<20;i++){
+	ans= !(a[i]+b[i]);
+	output= (a[i]*b[i]);
+printf("\n %d NOR %d = %d",
+               a[i], b[i], ans, ans);
+               
+ printf("\n %d  == %d = %d",
+               a[i], b[i], ans, output);		
+	if(a[i] ==1 && b[i] ==1){
+	ans =0;
+}
+	else{
+	ans =1;
+}
+printf("\n %d NAND %d = %d",
+               a[i], b[i], ans);
+
+
+}
+	return 1;
+		
+   
+    }
+
+
+
+
+
+
+
